1dfdcd6a9b6f6c500bcc94e4baf0096f8b1c4398
Pratheeka shetty <pratheekashetty123@gmail.com>
2024-11-06 20:25:20 +0530

    Invoice

---
+++ Employee_Project/demo/src/main/java/com/example/demo/EmployeeEntity.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class EmployeeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+	
+	@Column(nullable = false)
+    private String name;
+	
+	@Column(nullable = false)
+    private String department;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+    
+    
+}
