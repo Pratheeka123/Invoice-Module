86af2ba495b693b1462ae519eb55bfb624c6df6a
Pratheeka shetty <pratheekashetty123@gmail.com>
2024-11-06 20:27:36 +0530

    Delete Employee_Project/demo directory

--- Employee_Project/demo/src/main/java/com/example/demo/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepo extends JpaRepository<EmployeeEntity,Integer> {
-	    //Employee findByName(String name);
-	}
